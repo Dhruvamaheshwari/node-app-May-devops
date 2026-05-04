2741035c781e39761fc6ae2255bc14b0e9661639
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-05-04 15:08:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/** @format */
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello kya haal h jee");
+});
+
+app.listen(4000, () => console.log("server is running "));
