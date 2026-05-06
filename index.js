ebbcd50ffaa3a3e67f7bf7c830a26ca4d9c4425f
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-05-06 14:46:33 +0530

    update index.js to enhance home route response

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hello kya haal h jee");
+  res.send(`hello kya haal h jee <br> 
+        <a href="/home">home</a>
+    `);
 });
+
+app.get("/home", (req, res) => {
+  res.send(`This is the home page`);
+});
+
 
 app.listen(5000, () => console.log("server is running "));
